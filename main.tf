0667ebeebc6a0732ec8e176f79faf912550fb85a
Frank <frankgzmc@gmail.com>
2025-04-14 19:36:41 -0500

    Instalar NGINX al iniciar EC2

--- main.tf
+++ main.tf
@@ -7,4 +7,10 @@ provider "aws" {
 resource "aws_instance" "nginx-server" {
     ami = "ami-0440d3b780d96b29d"
     instance_type = "t3.micro"
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo yum install -y nginx
+                sudo systemctl enable nginx
+                sudo systemctl strat nginx
+                EOF
 } 
