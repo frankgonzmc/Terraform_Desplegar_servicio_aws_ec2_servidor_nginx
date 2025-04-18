1d5bec3a649c17bcf2b215432f923a3ab84f4a30
Frank <frankgzmc@gmail.com>
2025-04-18 18:37:38 -0500

    Agregar outputs de ip y dns de la instancia

--- main.tf
+++ main.tf
@@ -86,3 +86,15 @@ resource "aws_security_group" "nginx-server-sg" {
     }
 
 }
+
+###OUTPUTS###
+
+output "server_public_ip" {
+    description = "Direccion ip publica de la instancia EC2"
+    value = aws_instance.nginx-server.public_ip
+}
+
+output "server_public_dns" {
+    description = "DNS publico de la instancia EC2"
+    value = aws_instance.nginx-server.public_dns
+}
